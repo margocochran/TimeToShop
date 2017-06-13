c7343226640fbfff54ab469a009456f40a742143
Margo Cochran <margocochran@gmail.com>
2017-06-12 21:56:46 -0700

    Initial commit.

---
+++ TimeToShop/Cart.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeToShop
+{
+    /// <summary>
+    /// This is a shopping cart that stores the customer's goods for purchase.
+    /// </summary>
+    class Cart
+    {
+        #region Properties
+    
+        public int CustomerIdNumber { get; private set; }
+
+        public int EmailAddress { get; set; }
+
+        public decimal BalanceOwed { get; private set; }
+        #endregion
+
+        #region Methods
+        public void AddItem(string itemName, decimal itemPrice)
+        {
+            BalanceOwed += itemPrice; 
+        }
+        public void DeleteItem(string itemName, decimal itemPrice)
+        {
+            BalanceOwed -= itemPrice; 
+        }
+        public void CheckOut(string itemName, decimal balanceOwed); //is this the correct place for this? Or should it be another class?
+        #endregion
+    }
+}
